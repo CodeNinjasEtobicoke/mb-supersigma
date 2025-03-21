fe7f3ca41847bd0af0b9c256e6378316a08e05f2
Code Ninjas Etobicoke <codeninjasetobicoke@outlook.com>
2025-03-21 18:29:00 -0400

    ji
    hhhhh

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,15 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Objects shapes")]
+    public GameObject[] shapePrefabs;
+    [Header("Default sopawn time delay")]
+    public float spawnDelay = 2;
+    [Header("default spawn time")]
+    public float spawnTime = 3;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
-    void Update()
+    void Spawn()
     {
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
 
+    public void GameOver()
+    {
+        CancelInvoke("Spawn");
     }
 }
--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -6,15 +6,22 @@ public class Shape : MonoBehaviour
 {
     [Header("Rigidbody object")]
     public Rigidbody2D rb;
+    [Header("DEfault shrinking sped thing")]
+    public float shrinkSpeed = 3;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb.rotation = Random.Range(0, 360);
+        transform.localScale = Vector3.one * 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+        if (transform.localScale.x < 0.05f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
